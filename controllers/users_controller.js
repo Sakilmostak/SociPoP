669145be70bae335185b4cb662eb002fb261a075
Sakil Mostak <skmahim71@gmail.com>
2022-06-06 10:48:11 +0530

    added "Partials" to ejs

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,7 @@
 module.exports.profile = function(req,res){
-    res.end('<h1>User Profile</h1>')
+    return res.render('user_profile',{
+        title: "profile"
+    });
 }
 
 module.exports.edit = function(req,res){
