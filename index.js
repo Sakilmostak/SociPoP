f9defd233104180c1a806d853fffc096df19451b
Sakil Mostak <skmahim71@gmail.com>
2022-06-06 11:53:26 +0530

    added 'static files' and set it in ref to layouts

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const app = express();
 const port= 2500;
 const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
+
 // adding layouts in ejs
 app.use(expressLayouts);
+// extract style and scripts from subpages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 //use express router
 app.use('/',require('./routes'));
