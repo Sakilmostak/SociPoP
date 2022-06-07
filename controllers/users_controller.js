b4ca58bd4a211b66a32ca52715c7dfc8e18dddb9
Sakil Mostak <skmahim71@gmail.com>
2022-06-07 10:43:53 +0530

    added db to sign-in controller and created the route

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -30,29 +30,36 @@ module.exports.signIn= function(req,res){
 
 //get the signup data
 module.exports.create= function(req,res){
+    //handle if pass and confirm-pass doesnt match
     if(req.body.password!= req.body.confirm_password){
         // return to the same page
         return res.redirect('back');
     }
 
+    //find the user if exists
     User.findOne(
         {email: req.body.email},
         function(err, user){
+            //handling error
             if(err){
                 console.log('error in finding user in signing up');
                 return;
             }
 
+            // if user's email does not exists
             if(!user){
+                // creating a new profile using db
                 User.create(req.body, function(err, user){
                     if(err){
                         console.log('error in creating user while signing up');
+                        return;
                     }
 
                     return res.redirect('/users/sign-in');
                 });
             }
             else{
+                // if email exists
                 return res.redirect('back');
             }
         }
@@ -61,5 +68,32 @@ module.exports.create= function(req,res){
 
 //adding action when user sign-in 
 module.exports.createSession = function(req, res){
-    //do some
+    
+    //steps to authenticate
+    //find the user in db
+    User.findOne(
+        {email: req.body.email},
+        function(err,user){
+            if(err){
+                console.log('error in finding user in signing up');
+                return;
+            }
+
+            //if user found
+            if(user){
+                // handle password which doesn't match
+                if(user.password!= req.body.password){
+                    return res.redirect('back');
+                }
+
+                //handle session creation
+                res.cookie('user_id',user.id);
+                return res.redirect('/users/profile');
+            }
+            else{
+                //handle if user not found
+                return res.redirect('back');
+            }
+        }
+    )
 }
