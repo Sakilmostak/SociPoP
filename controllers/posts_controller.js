fa9874c43585d4bc65744414f838f651f67c5dee
Sakil Mostak <skmahim71@gmail.com>
2022-06-10 17:19:33 +0530

    added route, controller and ejs to delete a post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post= require('../models/post');
+const Comment = require('../models/comment');
 
 //adding action when directed to this controller
 module.exports.create= function(req, res){
@@ -17,4 +18,27 @@ module.exports.create= function(req, res){
             return res.redirect('back');
         }
     );
+}
+
+
+module.exports.destroy = function(req,res){
+    //params are provided as a query with the link
+    //eg. posts/delete/:id, here id is the param
+    Post.findById(req.params.id, function(err,post){
+        // .id means converting object _id into string
+        // which is good for comparison
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, function(err){
+                if(err){
+                    console.log(err);
+                }
+                return res.redirect('back');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
+    });
 }
